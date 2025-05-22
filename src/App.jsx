e1a1f402d532beb0845216914080cea65c5d1518
Szász István Zoltán <istvanszasz99@gmail.com>
2025-05-22 16:38:57 +0200

    Exercise 1: Create a React component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Import necessary modules from React library
+import React from 'react';
 
+// Import components for routing from react-router-dom library
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Import custom Navbar component
+import Navbar from './Components/Navbar/Navbar';
+
+// Import the Landing_Page component
+import Landing_Page from './Components/Landing_Page/Landing_Page';
+
+// Function component for the main App
 function App() {
-  const [count, setCount] = useState(0)
 
+  // Render the main App component
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+          {/* Set up BrowserRouter for routing */}
+          <BrowserRouter>
+            {/* Display the Navbar component */}
+            <Navbar/>
+
+            {/* Set up the Routes for different pages */}
+            <Routes>
+              {/* Home route for Landing_Page */}
+              <Route path="/" element={<Landing_Page />} />
+            </Routes>
+          </BrowserRouter>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+// Export the App component as the default export
+export default App;
